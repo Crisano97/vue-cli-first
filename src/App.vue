57ba9a87ad80566686b3adc97744cfb1131f93c1
Crisano97 <97keb6@gmail.com>
2022-07-14 16:02:50 +0200

    vue cli + bonus

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <FirstTitle />
+    <FirstHero />
+  </div>
+</template>
+
+<script>
+import FirstHero from './components/FirstHero.vue'
+import FirstTitle from './components/FirstTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstTitle,
+    FirstHero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: -8px;
+  padding: 0;
+  box-sizing: border-box;
+  margin-top: 50px;
+  
+}
+</style>
---
+++ src/components/FirstHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img id="jumbotron" src="https://static.geopop.it/wp-content/uploads/sites/32/2022/07/james-webb-prime-immagini.jpeg" alt="webb">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#jumbotron{
+    width: 100%;
+}
+
+</style>
